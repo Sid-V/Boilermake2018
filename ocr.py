d21dc640d479d0233184d3256ef9411b9a845479
Aahan Aggarwal <aahan.aggarwal99@gmail.com>
2018-10-20 17:31:18 -0400

    Removed junk

--- ocr.py
+++ ocr.py
@@ -13,13 +13,11 @@ vision_base_url = "https://eastus.api.cognitive.microsoft.com/vision/v2.0/"
 ocr_url = vision_base_url + "ocr"
 
 # Set image_url to the URL of an image that you want to analyze.
-image_url = "https://images.collegiatelink.net/clink/images/1f3abac2-2232-4f0a-bfa6-5d601b2f55dad87b412b-2515-48d7-8f64-82be101089d5.jpg?preset=med-w"
-image_url2 = "https://calendar.purdue.edu/calendar/displaymedia.aspx?whatToDo=picture&id=100697"
-image_url3 = "https://calendar.purdue.edu/calendar/displaymedia.aspx?whatToDo=picture&id=100697"
+image_url = "https://calendar.purdue.edu/calendar/displaymedia.aspx?whatToDo=picture&id=100697"
 
 headers = {'Ocp-Apim-Subscription-Key': subscription_key}
 params  = {'language': 'unk', 'detectOrientation': 'true'}
-data    = {'url': image_url3}
+data    = {'url': image_url}
 response = requests.post(ocr_url, headers=headers, params=params, json=data)
 response.raise_for_status()
 
@@ -40,8 +38,6 @@ for word in word_infos:
     newWord = re.sub('[^-A-Za-z0-9:\/]+','',word)
     wordList.append(newWord.lower())
 # remove punctuation
-
-#wordList = [''.join(c for c in s if c is not [':','\','/'] and c not in string.punctuation) for s in wordList]
 
 print(wordList)
 
