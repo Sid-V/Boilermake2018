2942acb6837d80df7f7efc8077a87508ae0b56da
kgaertne <33812693+kgaertne@users.noreply.github.com>
2018-10-20 16:26:06 -0400

    Add Basic Event

--- dataExtractor.py
+++ dataExtractor.py
@@ -10,6 +10,7 @@ monthAbbreviations = {'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'S
 date = ''
 time = ''
 locations = []
+titles = []
 
 def extractWords(list):
     global time
@@ -19,8 +20,9 @@ def extractWords(list):
         word = str(list[i])
         #checking date
         if re.match('[0-9]{1,2}/[0-9]{1,2}', word) is not None or re.match('[0-9]{1,2}\.[0-9]{1,2}', word) or re.match('[0-9]{1,2}\-[0-9]{1,2}', word):
-            date = word
-            continue
+            if list[i+1] != 'am' and list[i+1] != 'pm': # check if i+1 exists
+                date = word
+                continue
         if word[-3:] in monthAbbreviations:
             if list[i+1] is not None:
                 date += list[i+1]
@@ -43,30 +45,39 @@ def extractWords(list):
         elif word[-2:] == 'am' or word[-2:] == 'pm':
             extracted_time = word[:-2]
             if re.match('\d{1,2}\:\d{2}\-\d{1,2}\:\d{2}', extracted_time) is not None:
-                time = str(list[i-1][0:list[i-1].index('-')]) + word
-            elif re.match('\d{1,2}\-\d{1,2}', extracted_time) is not None:
-                time = str(list[i-1][0:list[i-1].index('-')]) + word
-            elif re.match('\d{1,2}\:\d{2}', extracted_time) is not None:
-                time = str(list[i-1]) + word
-            elif re.match('\d{1,2}', extracted_time) is not None:
-                time = str(list[i-1]) + word
-            else:
+                time = extracted_time + word[-2:]
                 continue
-            time = extracted_time + word[-2:]
-            continue
+                #time = str(list[i-1][0:list[i-1].index('-')]) + word
+            elif re.match('\d{1,2}\-\d{1,2}', extracted_time) is not None:
+                time = extracted_time + word[-2:]
+                continue
+                #time = str(list[i-1][0:list[i-1].index('-')]) + word
+            elif re.match('\d{1,2}\:\d{2}', extracted_time) is not None:
+                time = extracted_time + word[-2:]
+                continue
+                #time = str(list[i-1]) + word
+            elif re.match('\d{1,2}', extracted_time) is not None:
+                time = extracted_time + word[-2:]
+                continue
+            #else:
+            #    continue
 
         #checking location
         query_result = google_places.nearby_search(location='West Lafayette, United States', keyword=list[i])
         if len(query_result.places) > 0:
             location = query_result.places[0].name
-            if i < len(list) or list[i+1] is not None:
+            if i < len(list)-1:
                 locations.append(query_result.places[0].name + ' Room ' + list[i+1])
             else:
                 locations.append(query_result.places[0].name)
 
-list = ['6', 'am', 'LWSN', 'B160','go', 'slam']
+        if(i < 6):
+
+            titles.append(list[i])
+
+list = ['6-7', 'am', 'LWSN', 'B160','go', 'slam']
 extractWords(list)
+print(date)
 print(time)
 print(locations)
-
-
+print(titles)
