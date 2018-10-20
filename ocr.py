c26cb6ca3afc4fb6a5f5a727d6ce747af73612e8
Aahan Aggarwal <aahan.aggarwal99@gmail.com>
2018-10-20 17:42:17 -0400

    removed conflict

--- ocr.py
+++ ocr.py
@@ -13,13 +13,8 @@ vision_base_url = "https://eastus.api.cognitive.microsoft.com/vision/v2.0/"
 ocr_url = vision_base_url + "ocr"
 
 # Set image_url to the URL of an image that you want to analyze.
-<<<<<<< HEAD
-image_url = "https://images.collegiatelink.net/clink/images/1f3abac2-2232-4f0a-bfa6-5d601b2f55dad87b412b-2515-48d7-8f64-82be101089d5.jpg?preset=med-w"
-image_url2 = "https://calendar.purdue.edu/calendar/displaymedia.aspx?whatToDo=picture&id=100697"
-image_url3 = "https://engineering.purdue.edu/AAE/events/2017/20170829-SEDS-Callout-YPAC/Fall%202017%20SEDS%20Callout%20Flyer.jpg"
-=======
+
 image_url = "https://calendar.purdue.edu/calendar/displaymedia.aspx?whatToDo=picture&id=100697"
->>>>>>> d21dc640d479d0233184d3256ef9411b9a845479
 
 headers = {'Ocp-Apim-Subscription-Key': subscription_key}
 params  = {'language': 'unk', 'detectOrientation': 'true'}
@@ -49,12 +44,6 @@ print(wordList)
 # Write words into file for extraction
 extractor = InfoExtractor()
 extractor.extractWords(wordList)
-<<<<<<< HEAD
-print("Date: " + extractor.getDate())
-print("Time: " + extractor.getTime())
-print(extractor.getLocations())
-print(extractor.getTitles())
-=======
 
 date = extractor.getDate()
 time = extractor.getTime()
@@ -69,4 +58,3 @@ command = titles[0] + " at " + locations[0] + " on " + date + " at " + time
 print(command)
 event = Event()
 event.sendEvent(command) # send command to google calender event quick add
->>>>>>> d21dc640d479d0233184d3256ef9411b9a845479
