e3a03628a2f697c57303e1276c53e1cd23723317
Tarcan Gul <32048091+TarcanGul@users.noreply.github.com>
2018-10-20 20:58:13 -0400

    The roadmap

--- BoilermakeWebApp2018JS/BoilermakeWebApp2018JS/js/main.js
+++ BoilermakeWebApp2018JS/BoilermakeWebApp2018JS/js/main.js
@@ -2,9 +2,21 @@
 //Task: Open the camera after user clicks the button at the top, and after uploading, 
 // the python algorithm should run and set the locations, time and date. Calendar button will add 
 //to calendar
+
 var calendarButton = document.getElementById('calendar-button')
+var imageInput = document.getElementById('pictureInput')
 
 calendarButton.setEventListener("click", function ({
 
     //calendar button function
-});
+    });
+
+function handleFiles(image) {
+    //start python algorithm, probably send request.
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+       
+    };
+    xhttp.open("GET");
+    xhttp.send();
+}
